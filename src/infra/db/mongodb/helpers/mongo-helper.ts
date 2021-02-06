bc1b2fb6b91c6f98d146f3c5513c556a69cf096b
Marcos Azevedo <marcos.azevedo@izap.com.br>
2021-02-05 22:49:55 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -16,5 +16,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name)
+  },
+
+  map: (collection: any): any => {
+    const { _id, ...rest } = collection
+    return Object.assign({}, rest, { id: _id })
   }
 }
