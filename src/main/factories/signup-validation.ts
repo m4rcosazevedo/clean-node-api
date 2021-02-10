981445e24d0167a32dbfb789ec1945d5b34490f8
Marcos Azevedo <marcos.azevedo@izap.com.br>
2021-02-10 11:47:06 -0300

    test: add tests to CompareFieldsValidation

--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,7 +1,7 @@
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
-import { CompareFieldValidation } from '../../presentation/helpers/validators/compare-field-validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { EmailValidation } from '../../presentation/helpers/validators/email-validation'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
@@ -11,7 +11,7 @@ export const makeSignUpValidation = (): ValidationComposite => {
     validations.push(new RequiredFieldValidation(field))
   }
 
-  validations.push(new CompareFieldValidation('password', 'passwordConfirmation'))
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
 
   return new ValidationComposite(validations)
