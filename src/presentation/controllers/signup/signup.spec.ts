5ccc4d448f454c2ee55d8e98367c9a2750d621bb
Marcos Azevedo <marcos.azevedo@izap.com.br>
2021-02-09 22:26:12 -0300

    test: ensure SignUpController calls validation with correct value

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -3,7 +3,8 @@ import {
   AddAccount,
   AddAccountModel,
   EmailValidator,
-  HttpRequest
+  HttpRequest,
+  Validation
 } from './signup-protocols'
 import { SignUpController } from './signup'
 import { InvalidParamError, MissingParamError, ServerError } from '../../erros'
@@ -28,6 +29,15 @@ const makeAddAccount = (): AddAccount => {
 
   return new AddAccountStub()
 }
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (_input: any): Error {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'valid_id',
@@ -49,16 +59,20 @@ interface SutTypes {
   sut: SignUpController
   emailValidatorStub: EmailValidator
   addAccountStub: AddAccount
+  validationStub: Validation
 }
 
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator()
   const addAccountStub = makeAddAccount()
-  const sut = new SignUpController(emailValidatorStub, addAccountStub)
+  const validationStub = makeValidation()
+  const sut = new SignUpController(emailValidatorStub, addAccountStub, validationStub)
+
   return {
     sut,
     emailValidatorStub,
-    addAccountStub
+    addAccountStub,
+    validationStub
   }
 }
 
@@ -176,5 +190,13 @@ describe('SignUp Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(ok(makeFakeAccount()))
+  })
+
+  test('Should call Validation with correct value', async () => {
+    const { sut, validationStub } = makeSut()
+    const validationSpy = jest.spyOn(validationStub, 'validate')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(validationSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
