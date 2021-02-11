7f556e6a60e1c6576b62f270b59c451b777610b2
Marcos Azevedo <marcos.azevedo@izap.com.br>
2021-02-11 13:29:19 -0300

    refactor: move bcrypt-adapter files to a folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { makeSignUpValidation } from './signup-validation'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { Controller } from '../../../presentation/protocols'
