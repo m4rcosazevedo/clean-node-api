24b78fa601bca78c04e3ed55cba7173fdb188d7b
Marcos Azevedo <m4rcos.azevedo@gmail.com>
2021-02-17 23:36:45 -0300

    fix: add omit date

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,6 +1,6 @@
 import { SurveyModel } from '@/domain/models/survey-model'
 
-export type AddSurveyModel = Omit<SurveyModel, 'id'>
+export type AddSurveyModel = Omit<SurveyModel, 'id'|'date'>
 
 export interface AddSurvey {
   add (data: AddSurveyModel): Promise<void>
