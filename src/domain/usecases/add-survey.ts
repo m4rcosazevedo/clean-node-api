df70700bd86ebfdb703caa08985dbca45ec52aea
Marcos Azevedo <m4rcos.azevedo@gmail.com>
2021-02-18 08:56:38 -0300

    refactor: change AddSurvey to insert a date also

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,6 +1,6 @@
 import { SurveyModel } from '@/domain/models/survey-model'
 
-export type AddSurveyModel = Omit<SurveyModel, 'id'|'date'>
+export type AddSurveyModel = Omit<SurveyModel, 'id'>
 
 export interface AddSurvey {
   add (data: AddSurveyModel): Promise<void>
